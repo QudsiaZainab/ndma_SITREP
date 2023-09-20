87fd1add95cfe5c0a5198626c8337489a50245a2
QudsiaZainab <95419016+QudsiaZainab@users.noreply.github.com>
2023-09-20 19:00:40 +0500

    Add files via upload

--- src/components/EarthquakeInputSeverity/index.js
+++ src/components/EarthquakeInputSeverity/index.js
@@ -8,7 +8,7 @@ export const EarthquakeInputSeverity = ({ data }) => {
     const [selectedTehsil, setSelectedTehsil] = useState('');
     const [selectedUC, setSelectedUC] = useState('');
     const [earthquakeInputSeverity, setEathquakeInputSeverity] = useState('');
-    // const [province, setProvince] = useState('');
+    const [intensity, setIntensity] = useState('');
 
     const handleProvinceChange = (event) => {
         const provinceName = event.target.value;
@@ -37,7 +37,7 @@ export const EarthquakeInputSeverity = ({ data }) => {
     return (
         <div className="overflow-y-scroll h-screen pb-12">
             <form onSubmit={sendResponse} className="px-4 my-12 max-w-2xl mx-auto space-y-4 bg-gray-100 border border-gray-400 py-6 rounded-lg px-10">
-            <h1 className="text-xl font-bold text-center">INPUT SEVERITY OF EARTHQUAKE</h1>
+            <h1 className="text-xl font-bold text-center">EARTHQUAKE SHAKE INTENSITY</h1>
                 <div><label>Select Province:</label>
                     <select required value={selectedProvince} className="border border-gray-400 block py-2 px-4 w-full rounded focus:outline-none focus:border-teal-500" onChange={handleProvinceChange}>
                         <option value="">Select Province</option>
@@ -69,8 +69,7 @@ export const EarthquakeInputSeverity = ({ data }) => {
                         ))}
                     </select></div>
 
-                            <div>
-                    <label className="">Select Input Severity of Earthquake:</label>
+                    <div><label className="">Select Input Severity of Earthquake:</label>
                         <select
                             className="border border-gray-400 block py-2 px-4 w-full rounded focus:outline-none focus:border-teal-500"
                             value={earthquakeInputSeverity}
@@ -83,7 +82,10 @@ export const EarthquakeInputSeverity = ({ data }) => {
                                     {inputSeverity}
                                 </option>
                             ))}
-                        </select>
+                        </select></div>
+                        <div >
+                                <label className="">Enter Earthquake Shake Intensity:</label>
+                                <input className="border border-gray-400 block py-2 mt-0 px-4 w-full ml-0 rounded focus:outline-none focus:border-teal-500" placeholder='Enter Earthquake Shake Intensity' type = "number" onChange={(e) => {setIntensity(e.target.value) }} required/>
                             </div>
                 <div className="flex justify-center items-center">
                     <button type="submit" className="bg-gray-300 hover:bg-gray-400 font-bold py-2 px-4 rounded m-6" >Submit</button>
